79fe0434e83b7300435f42cdc5f2e837970b3c2f
pedrohercules0810 <pedrosouzagrangeiro@gmail.com>
2025-01-14 20:10:44 -0300

    Deixando a seleção de carta mais consistente

--- Programa/main.js
+++ Programa/main.js
@@ -22,21 +22,24 @@ function criaBaralho() {
     return baralho;
 }
 
-function cartaAleatoria() {
-    return Math.floor(Math.random() * (51 - 0)) + 0;
+
+
+function cartaAleatoria(size) {
+    return Math.floor(Math.random() * (size)) ;
 }
 
 let baralho = criaBaralho();
 //mostrando as cartas comunitarias
 console.log(baralho);
-let random = cartaAleatoria()
+console.log(baralho.length);
+let random = cartaAleatoria(baralho.length)
 
 let cartaParaRemover = baralho[random]; // Carta a ser removida
 
 console.log(baralho[random]);
 
-
 // Filtrando o baralho para excluir a carta
 baralho = baralho.filter(carta => carta.valor !== cartaParaRemover.valor || carta.naipe !== cartaParaRemover.naipe);
 
 console.log(baralho);
+console.log(baralho.length);
